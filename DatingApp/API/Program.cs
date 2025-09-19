0e8130fd47d2bec1d24a270a4598993e435e9097
Dju <djurao@hotmail.com>
2025-09-19 18:57:16 +0200

    Progress

--- DatingApp/API/Program.cs
+++ DatingApp/API/Program.cs
@@ -11,12 +11,28 @@ builder.Services.AddDbContext<AppDbContext>(options =>
     // Use SQLite database with connection string from configuration(added to appsettings.Development.json)
     options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection"));
 });
-// Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
-builder.Services.AddOpenApi();
+
+// Add Swagger services
+builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
 
-app.MapOpenApi();
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseDeveloperExceptionPage();
+
+    // Enable middleware to serve generated Swagger as a JSON endpoint.
+    app.UseSwagger();
+
+    // Enable middleware to serve swagger-ui (HTML, JS, CSS, etc.),
+    // specifying the Swagger JSON endpoint.
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "My API V1");
+    });
+}
 
 app.MapControllers();
 
--- DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
+++ DatingApp/API/obj/Debug/net9.0/API.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("API")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ca600183276100d59b675cea69b2b2020fcac005")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+ff7f629695954df0259fa7537f9f2f2dba67fc0f")]
 [assembly: System.Reflection.AssemblyProductAttribute("API")]
 [assembly: System.Reflection.AssemblyTitleAttribute("API")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
